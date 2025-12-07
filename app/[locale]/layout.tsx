0843a322c345e52eb8de225b10a61767b280aba4
itsdamnrj <rjretritavillar@gmail.com>
2025-12-07 14:45:40 +0800

    fix: type error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,7 +4,6 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { locales } from "@/i18n/request";
-// @ts-expect-error - CSS imports don't have type declarations
 import "../globals.css";
 
 const inter = Inter({
